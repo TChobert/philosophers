ba00735d87b3fafc56442b399242af8b113a90a4
Thomas Chobert <tchobert@made-f0Dr10s19.clusters.42paris.fr>
2024-10-15 19:53:51 +0200

    15/09 parsing debut

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,12 +6,21 @@
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 13:43:10 by tchobert          #+#    #+#             */
-/*   Updated: 2024/10/15 13:53:06 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:52:21 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOPHERS_H
 # define PHILOSOPHERS_H
+
+// INCLUDES //
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+
+# include "libft.h"
 
 // DEFINES //
 
@@ -19,7 +28,32 @@
 
 // TYPEDEFS //
 
+// ENUMS //
+
+typedef enum e_parsing_error_status
+{
+	INVALID_PHILOS_NUMBER,
+	INVALID_
+}			t_parsing_error_status;
+
+typedef enum e_input_status
+{
+	VALID_INPUT = -1,
+	INVALID_INPUT
+}			t_input_status;
+
+typedef enum e_argument_status
+{
+	INVALID_ARGUMENT = -1,
+	VALID_ARGUMENT
+}			t_argument_status;
+
 // STRUCTS //
+
+typedef struct s_input_data
+{
+	unsigned long	philos_number;
+}				t_input_data;
 
 typedef struct s_philosopher
 {
--- sources/parsing/arguments_parser.c
+++ sources/parsing/arguments_parser.c
@@ -6,13 +6,28 @@
 /*   By: tchobert <tchobert@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 14:08:41 by tchobert          #+#    #+#             */
-/*   Updated: 2024/10/15 15:04:43 by tchobert         ###   ########.fr       */
+/*   Updated: 2024/10/15 19:53:00 by tchobert         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-// t_input_type	arguments_parser(char **arguments)
-// {
+t_argument_status	check_philos_number_validity(char *philos_number,
+						t_input_data *input_data)
+{
+	if (philos_number[0] == '-')
+	{
+		ft_dprintf(STDERR_FILENO, "Invalid number of philosophers. Please"
+			" enter a positive value\n");
+		return (INVALID_ARGUMENT);
+	}
+}
 
-// }
+t_input_status	arguments_parser(char **arguments)
+{
+	t_input_data	input_data;
+
+	if (check_philos_number_validity(arguments[0], &input_data)
+		== INVALID_ARGUMENT)
+		return (INVALID_INPUT);
+}
